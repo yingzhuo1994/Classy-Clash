7abf37978021bb53a15ff6cad96dc6b99383d847
yingzhuo <yingzhuo1994@gmail.com>
2021-10-01 13:19:23 -0500

    Movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -11,6 +12,8 @@ int main()
 
     // map variables
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -19,9 +22,19 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
 
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
 
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+  
 
-        Vector2 mapPos{0.0, 0.0};
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         // stop drawing
