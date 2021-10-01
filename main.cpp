6fdfabf21c4e02f9738905355201fc1465b78ffe
yingzhuo <yingzhuo1994@gmail.com>
2021-10-01 18:13:30 -0500

    Animate the character

--- main.cpp
+++ main.cpp
@@ -15,11 +15,22 @@ int main()
     Vector2 mapPos{0.0, 0.0};
     float speed{4.0};
 
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");   
+    Texture2D knight = knight_idle;
     Vector2 knightPos{
         windowDimensions[0] / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0),
         windowDimensions[1] / 2.0f - 4.0f * (0.5f * (float)knight.height)
     };
+
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
+
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -38,13 +49,27 @@ int main()
         {
             // set mapPos = mapPos - direction
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+        }
+        else
+        {
+            knight = knight_idle;
         }
   
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrames) frame = 0;
+        }
         // draw the character
-        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
         Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
         DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
         // stop drawing
