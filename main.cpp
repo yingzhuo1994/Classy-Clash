738f5b350adf399004ea48a0585c78b24ed37c58
yingzhuo <yingzhuo1994@gmail.com>
2021-10-01 13:35:42 -0500

    Draw the character

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@ int main()
     Vector2 mapPos{0.0, 0.0};
     float speed{4.0};
 
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        windowDimensions[0] / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0),
+        windowDimensions[1] / 2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -34,9 +40,13 @@ int main()
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
         }
   
-
+        // draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
+        // draw the character
+        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
         // stop drawing
         EndDrawing();
     }
