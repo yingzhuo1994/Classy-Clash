fe3eb6c79740454b913a1d7ed14839b2ba4ede9f
yingzhuo <yingzhuo1994@gmail.com>
2021-10-02 13:47:42 -0500

    Check the map bounds

---
+++ Character.h
@@ -0,0 +1,29 @@
+#include "raylib.h"
+
+class Character
+{
+public:
+    Character();
+    Vector2 getWorldPos() { return worldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
+    void undoMovement();
+
+private:
+    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1};
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames{6};
+    const float updateTime{1.f / 12.f};
+    const float speed{4.f};
+    float width{};
+    float height{};
+};
--- main.cpp
+++ main.cpp
@@ -1,71 +1,6 @@
 #include "raylib.h"
 #include "raymath.h"
-
-class Character
-{
-public:
-    Vector2 getWorldPos() { return worldPos; }
-    void setScreenPos(int winWidth, int winHeight);
-    void tick(float deltaTime);
-
-private:
-    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    // 1 : facing right, -1 : facing left
-    float rightLeft{1};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    const int maxFrames{6};
-    const float updateTime{1.f / 12.f};
-    const float speed{4.f};
-};
-
-void Character::setScreenPos(int winWidth, int winHeight)
-{
-    screenPos = {
-        (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0),
-        (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)    
-    };
-}
-
-void Character::tick(float deltaTime)
-{
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-    if (IsKeyDown(KEY_D)) direction.x += 1.0;
-    if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-    if (IsKeyDown(KEY_S)) direction.y += 1.0;
-
-    if (Vector2Length(direction) != 0.0)
-    {
-        // set worldPos = worldPos + direction
-        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-        texture = run;
-    }
-    else
-    {
-        texture = idle;
-    }    
-
-    // update animation frame
-    runningTime += deltaTime;
-    if (runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames) frame = 0;
-    }
-
-    // draw the character
-    Rectangle source{frame * (float)texture.width/6.f, 0.f, rightLeft * (float)texture.width/6.f, (float)texture.height};
-    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width/6.0f, 4.0f * (float)texture.height};
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
+#include "Character.h"
 
 int main()
 {
@@ -79,6 +14,7 @@ int main()
     // map variables
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos{0.0, 0.0};
+    const float mapScale{4.0};
 
     Character knight;
     knight.setScreenPos(windowDimensions[0], windowDimensions[1]);
@@ -93,9 +29,16 @@ int main()
         mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
         // draw the map
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
         knight.tick(GetFrameTime());
-
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f
+         || knight.getWorldPos().y < 0.f
+         || knight.getWorldPos().x + windowDimensions[0] > map.width * mapScale
+         || knight.getWorldPos().y + windowDimensions[1] > map.height * mapScale) 
+        {
+            knight.undoMovement();
+        }
         // stop drawing
         EndDrawing();
     }
