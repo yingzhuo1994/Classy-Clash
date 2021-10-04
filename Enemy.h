2d8341064c071d132718f85447bf0c0db01ff34d
yingzhuo <yingzhuo1994@gmail.com>
2021-10-04 13:46:18 -0500

    Variable addresses

--- Enemy.h
+++ Enemy.h
@@ -1,11 +1,13 @@
 #include "raylib.h"
 #include "BaseCharacter.h"
+#include "Character.h"
 
 class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_testure);
     virtual void tick(float deltaTime) override;
+    void setTarget(Character* character){ target = character;};
 private:
-
+    Character* target;
 };
--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ int main()
         LoadTexture("characters/goblin_idle_spritesheet.png"),
         LoadTexture("characters/goblin_run_spritesheet.png")
     };
+    goblin.setTarget(&knight);
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
